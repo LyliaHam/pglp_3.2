91c7675eefae9f59d5f45001ec729738f5665b77
Utilisateur <user@debian.arcahe.ovh>
2020-05-24 22:10:46 +0200

    Reorganisation

---
+++ pglp/src/main/java/lylia/Employe.java
@@ -0,0 +1,25 @@
+public class Employe extends EmployeAbstractClass {
+	
+	protected float salaire;
+	protected int anciennete;
+
+	public Employe (float salaire, int anciennete){
+		this.salaire = salaire;
+		this.anciennete = anciennete;
+	}
+	
+	public double getSalaire (double s) {
+		s = this.salaire;
+		return s;
+	}
+
+	public int getAnciennete (int a) {
+		a = this.anciennete;
+		return a;
+	}
+
+	public double salaireEmploye() {
+		return 1500 + (20 * anciennete);
+	}
+
+}
